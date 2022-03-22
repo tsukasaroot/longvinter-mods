8dde75964a17985cb9154e9e85d2151a4a20fa73
Jordan <jordan.rouco-miguez@epitech.eu>
2022-03-22 17:35:51 +0100

    updated script + list

--- stringifier.php
+++ stringifier.php
@@ -1,8 +1,21 @@
 <?php
 
-$array = [ 'url' => $argv[1]];
+if ($argc === 2 && $argv[1] === 'view') {
+	$mods = file_get_contents('modules-list.json');
+	$mods = json_decode($mods, true);
+	var_dump($mods);
+	exit;
+}
 
-$mods = json_encode($array);
+if ($argc !== 3)
+	exit;
+
+$mods = file_get_contents('modules-list.json');
+$mods = json_decode($mods, true);
+
+$mods[$argv[1]] = $argv[2];
+
+$mods = json_encode($mods);
 
 file_put_contents('modules-list.json', $mods);
 
